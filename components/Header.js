c2a5b160c850c56b577825582585d5cd28e1c0be
adrienbannwarth <adrien.bann@gmail.com>
2020-05-14 14:34:15 +0200

    change header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const ImgLogo = require("../assets/img/ssdp_logo.png");
+const IconMenu = require("../assets/icons/menu.svg");
 
 export default function Header({ navigation }) {
   return (
     <View style={styles.header}>
-      <Button
-        title="Menu"
-        onPress={() => navigation.toggleDrawer()} />
+      <TouchableOpacity
+        style={styles.btnDrawer}
+        onPress={() => navigation.toggleDrawer()}>
+        <Image
+          style={styles.logo}
+          source={IconMenu}
+          resizeMode="contain" />
+      </TouchableOpacity>
+      <Image
+        style={styles.logo}
+        source={ImgLogo}
+        resizeMode="contain" />
+      <TouchableOpacity
+        style={styles.btnDrawer}
+        onPress={() => navigation.toggleDrawer()}>
+        <Text>Cloche</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +32,16 @@ export default function Header({ navigation }) {
 const styles = StyleSheet.create({
   header: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     width: '100%',
+    paddingTop: 20,
     backgroundColor: '#109CF1'
+  },
+  logo: {
+    width: '60%'
+  },
+  btnDrawer: {
   }
 });
